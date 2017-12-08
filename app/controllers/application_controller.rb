f4c9cc6a70d30598c469a5aaf903f08d85f97614
Jesse Y <jesse@arrayschool.com>
2017-12-08 11:56:31 -0700

    added all kinds of stuff , WOOT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     lists_path
   end
+
+  protected
+  def require_login
+    redirect_to new_user_session_path, notice: 'You must be logged in' unless user_signed_in?
+  end
 end
